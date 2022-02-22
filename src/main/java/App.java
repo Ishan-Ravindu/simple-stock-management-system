dfe3825d7b9c7f83ef9ea01ae7a7182eed0841d2
sarithhasarinda <86558774+sarithhasarinda@users.noreply.github.com>
2022-02-22 16:32:09 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,15 @@
 import auth.LoginFrame;
+/*
+IM/2019/066-Sarith Hasarinda
+IM/2019/020-Ishan Ravindu
+IM/2019/026-Tharuka Wikum
+IM/2019/017-Racshan C
+IM/2019/105-Chamodi Sandeepani
+IM/2019/084-Kavisha Amarsinghe
+IM/2019/017-Nimtara Munasinghe
+IM/2019/017-Hiruni Keenavinna
+IM/2019/017-Gauri Wathsala
+*/
 
 public class App 
 {
